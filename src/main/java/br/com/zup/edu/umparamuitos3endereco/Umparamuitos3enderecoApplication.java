d963caa4482f27f2dbb4c8c6b6c2e654154c03c1
Denes <jose.araujo@zup.com.br>
2022-04-07 13:43:22 -0300

    criando a aplicacao

---
+++ src/main/java/br/com/zup/edu/umparamuitos3endereco/Umparamuitos3enderecoApplication.java
@@ -0,0 +1,13 @@
+package br.com.zup.edu.umparamuitos3endereco;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Umparamuitos3enderecoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Umparamuitos3enderecoApplication.class, args);
+	}
+
+}
